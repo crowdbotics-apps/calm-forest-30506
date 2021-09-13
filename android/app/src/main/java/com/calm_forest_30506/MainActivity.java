c22c1ae5e74b8652facfce7e4e818e7bd88f0444
Crowdbotics <team@crowdbotics.com>
2021-09-13 15:43:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/calm_forest_30506/MainActivity.java
@@ -0,0 +1,15 @@
+package com.calm_forest_30506;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "calm_forest_30506";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "calm_forest_30506Identifier",
+  url: "https://calm_forest_30506Identifier.botics.co",
+  api: "https://calm_forest_30506Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
